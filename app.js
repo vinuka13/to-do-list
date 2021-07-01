b14ab862a33beac488561546eff9f3bc2bf4067c
unknown <pussepitiyavinuka@gmail.com>
2021-07-01 19:45:46 +0530

    add gitignore, procfile and ports

--- app.js
+++ app.js
@@ -125,9 +125,10 @@ app.get("/:paramName", function(req, res){
   })
 })
 
-
-
-
-app.listen(3000, function(){
-  console.log("sever is fucking")
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function(){
+  console.log("sever is running")
 })
